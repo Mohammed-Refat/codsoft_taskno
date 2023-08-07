9dca2fa4aedd2e6774341542d339e7e10eb5af3b
Mohamed Refat <hamadarefat29@gmail.com>
2023-08-07 23:16:16 +0300

    Add files via upload
    Update JS

---
+++ Wheels/js/main.js
@@ -0,0 +1,206 @@
+
+let heroBg = document.querySelector('.hero');
+
+// Animation function
+function animateHeroBackground() {
+    heroBg.style.backgroundImage = "url(imgs/bg-light.jpg)";
+
+    setTimeout(() => {
+        heroBg.style.backgroundImage = "url(imgs/bg.jpg)";
+    }, 1000);
+}
+
+// Set the interval for the animation
+setInterval(animateHeroBackground, 2200);
+
+
+
+// Get Slider Items | Array.form [ES6 Feature]
+var sliderImages = Array.from(document.querySelectorAll('.slider-container img'));
+
+// Get Number Of Slides
+var slidesCount = sliderImages.length;
+
+// Set Current Slide
+var currentSlide = 1;
+
+// Slide Number Element
+var slideNumberElement = document.getElementById('slide-number');
+
+// Previous and Next Buttons
+var nextButton = document.getElementById('next');
+var prevButton = document.getElementById('prev');
+
+// Handle Click on Previous and Next Buttons
+nextButton.onclick = nextSlide;
+prevButton.onclick = prevSlide;
+
+// Create The Main UL Element
+var paginationElement = document.createElement('ul');
+
+// Set ID On Created Ul Element
+paginationElement.setAttribute('id', 'pagination-ul');
+
+// Create List Items Based On Slides Count
+for (var i = 1; i <= slidesCount; i++) {
+
+  // Create The LI
+  var paginationItem = document.createElement('li');
+
+  // Set Custom Attribute
+  paginationItem.setAttribute('data-index', i);
+
+  // Set Item Content
+  paginationItem.appendChild(document.createTextNode(i));
+
+  // Append Items to The Main Ul List
+  paginationElement.appendChild(paginationItem);
+
+}
+
+// Add The Created UL Element to The Page
+document.getElementById('indicators').appendChild(paginationElement);
+
+// Get The New Created UL
+var paginationCreatedUl = document.getElementById('pagination-ul');
+
+// Get Pagination Items | Array.form [ES6 Feature]
+var paginationsBullets = Array.from(document.querySelectorAll('#pagination-ul li'));
+
+// Loop Through All Bullets Items
+for (var i = 0; i < paginationsBullets.length; i++) {
+
+  paginationsBullets[i].onclick = function () {
+
+    currentSlide = parseInt(this.getAttribute('data-index'));
+
+    theChecker();
+
+  }
+
+}
+
+// Trigger The Checker Function
+theChecker();
+
+// Next Slide Function
+function nextSlide() {
+
+  if (nextButton.classList.contains('disabled')) {
+
+    // Do Nothing
+    return false;
+
+  } else {
+
+    currentSlide++;
+
+    theChecker();
+
+  }
+
+}
+
+// Previous Slide Function
+function prevSlide() {
+
+  if (prevButton.classList.contains('disabled')) {
+
+    // Do Nothing
+    return false;
+
+  } else {
+
+    currentSlide--;
+
+    theChecker();
+
+  }
+
+}
+
+// Create The Checker Function
+function theChecker() {
+
+    const carTypes = [
+        "Sedan",
+        "SUV (Sport Utility Vehicle)",
+        "Hatchback",
+        "Coupe",
+        "Electric Vehicle (EV)",
+        "Ferrari",
+
+      ];
+  // Set The Slide Number
+  slideNumberElement.textContent = carTypes[currentSlide-1];
+
+  // Remove All Active Classes
+  removeAllActive();
+
+  // Set Active Class On Current Slide
+  sliderImages[currentSlide - 1].classList.add('active');
+
+  // Set Active Class on Current Pagination Item
+  paginationCreatedUl.children[currentSlide - 1].classList.add('active');
+
+  // Check if Current Slide is The First
+  if (currentSlide == 1) {
+
+    // Add Disabled Class on Previous Button
+    prevButton.classList.add('disabled');
+
+  } else {
+
+    // Remove Disabled Class From Previous Button
+    prevButton.classList.remove('disabled');
+
+  }
+
+  // Check if Current Slide is The Last
+  if (currentSlide == slidesCount) {
+
+    // Add Disabled Class on Next Button
+    nextButton.classList.add('disabled');
+
+  } else {
+
+    // Remove Disabled Class From Next Button
+    nextButton.classList.remove('disabled');
+
+  }
+
+}
+
+// Remove All Active Classes From Images and Pagination Bullets
+function removeAllActive() {
+
+  // Loop Through Images
+  sliderImages.forEach(function (img) {
+
+    img.classList.remove('active');
+
+  });
+
+  // Loop Through Pagination Bullets
+  paginationsBullets.forEach(function (bullet) {
+
+    bullet.classList.remove('active');
+
+  });
+
+}
+
+// Empty Refresh 
+window.onload = function() {
+  // Get references to the input fields and the textarea
+  var nameField = document.querySelector('.name input');
+  var emailField = document.querySelector('.email input');
+  var subjectField = document.querySelector('.subject input');
+  var messageField = document.querySelector('.textarea textarea');
+
+  // Clear the fields
+  nameField.value = '';
+  emailField.value = '';
+  subjectField.value = '';
+  messageField.value = '';
+};
